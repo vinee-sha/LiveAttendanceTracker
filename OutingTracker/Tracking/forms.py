4de37ee5143961c8deeaf0666becb0c0ed47a7df
vinee-sha <50228002+vinee-sha@users.noreply.github.com>
2021-06-18 20:22:14 +0530

    Delete OutingTracker directory

--- OutingTracker/Tracking/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import uploadFile
-from django import forms
-
-class uploadfileform(forms.ModelForm):
-    	class Meta:
-            model=uploadFile
-            fields=('File_to_upload',)
